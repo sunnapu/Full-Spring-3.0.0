f90f07f1490766d2f1b32102dbf22597e1a24295
sunanan <anan.sun@gmail.com>
2014-05-22 15:17:36 -0600

    ver 1.0

---
+++ src/main/java/com/cgi/service/impl/PasswordTokenManager.java
@@ -0,0 +1,19 @@
+package com.cgi.service.impl;
+
+import com.cgi.model.User;
+
+
+public interface PasswordTokenManager {
+
+    /**
+     * {@inheritDoc}
+     */
+    String generateRecoveryToken(User user);
+
+    /**
+     * {@inheritDoc}
+     */
+    boolean isRecoveryTokenValid(User user, String token);
+
+    void invalidateRecoveryToken(User user, String token);
+}
